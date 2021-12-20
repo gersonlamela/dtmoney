2b01b8de3d015479980f1174076269925be41354
Gerson Lamela <gersonlamela7@gmail.com>
2021-12-20 02:34:45 +0000

    Model and Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import Modal from 'react-modal';
+import { useState } from 'react';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true)
+  }
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransitionModal={handleOpenNewTransactionModal} />
       <Dashboard/>
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
