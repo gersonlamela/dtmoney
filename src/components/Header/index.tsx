8c08ae5a7e7779003802ed9bc48088dd1928c125
Gerson Lamela <gersonlamela7@gmail.com>
2021-12-20 00:34:37 +0000

    Consumindo API

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,13 @@
-export function Header(){
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
+export function Header() {
   return (
-    <header>
-      Header
-    </header>
-  )
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button">Nova transação</button>
+      </Content>
+    </Container>
+  );
 }
